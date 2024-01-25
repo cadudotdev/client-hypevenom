254f87764a2daed0d7ef5564c0d3b448ba6b5cf2
carlos.santos <carlos.santos@neomind.com.br>
2024-01-25 20:31:13 -0300

    feat: Desenvolver a tela de site em desenvolvimento.

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -6,15 +6,24 @@ import { Artist } from '../artist/Artist';
 import { Artists } from '../artists/Artists';
 import { HeaderSeparator } from '../headerSeparator/HeaderSeparator';
 import { Main } from '../main/Main';
+import { Development } from '../development/Development';
+
+const devMode = true;
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <>
-        <DrawerAppBar />
-        <HeaderSeparator />
-        <Main />
+        {devMode ? (
+          <Development />
+        ) : (
+          <>
+            <DrawerAppBar />
+            <HeaderSeparator />
+            <Main />
+          </>
+        )}
       </>
     ),
   },
